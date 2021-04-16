de6f5992f19e64f5da4310d0bffc4f19790210fe
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2021-04-16 11:39:38 +0530

    initial commit

--- src/state/reducers/index.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import repositoryReducer from "./repositoriesReducer";
-
-const reducers = combineReducers({
-  repository: repositoryReducer,
-});
-
-export default reducers;
